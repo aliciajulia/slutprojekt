ee6d63a529c0ab00ff559a938d27eba74f7b1df3
Alicia Broberg <alicia.broberg@gmail.com>
2015-04-04 15:20:40 +0200

    20150330

--- adminIndex.php
+++ adminIndex.php
@@ -43,11 +43,11 @@ if ($_SESSION['inlog'] == 1) {
 
                 <div id="nav">
                     <ul>
-                        <li ><a class="topLeft active" href="index.html">Homepage</a></li>
+                        <li ><a class="topLeft active" href="adminIndex.php">Homepage</a></li>
                         <li><a href="om.html">About</a></li>
                         <li><a href="foretagside.html">Business Idea</a>
                             <ul>
-                                <li><a href="aterforsaljare.html">Resellers</a></li>
+                                <li><a href="adminAterforsaljare.php">Resellers</a></li>
                                 <li><a href="utbud.html">Product Range</a></li>
                             </ul>
                         </li>
@@ -83,11 +83,9 @@ if ($_SESSION['inlog'] == 1) {
                         <input type="hidden" name="id" value="h1-1">
                         <input type="hidden" name="index" value="index">
                     </form>';
-                        
-                        
 
                         echo '<form method="POST" action="doUpdate.php">
-                            <input type="text" style="width:330px; height:270px;" rows='17' cols="40" name="content" value="'.$data["1"]["content"].'">
+                            <input type="text" style="width:300px; height:270px;" rows="17" cols="40" name="content" value="'.$data["1"]["content"].'">
                             <input type="hidden" name="id" value="p-1">
                             <input type="hidden" name="index" value="index">
                         </form>';
@@ -98,14 +96,44 @@ if ($_SESSION['inlog'] == 1) {
 
                     </div>
                     <div class="index_tre">
-                        <h1>Our vison</h1>
-                        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some </p>
+                         <?php
+                        echo '<form method="POST" action="doUpdate.php">
+                        <input type="text" name="content" value="'.$data["2"]["content"].'">
+                        <input type="hidden" name="id" value="h1-1">
+                        <input type="hidden" name="index" value="index">
+                    </form>';
+                        
+                        
+
+                        echo '<form method="POST" action="doUpdate.php">
+                            <input type="text" style="width:300px; height:270px;" rows="17" cols="40" name="content" value="'.$data["3"]["content"].'">
+                            <input type="hidden" name="id" value="p-1">
+                            <input type="hidden" name="index" value="index">
+                        </form>';
+                                ?>
+                        <!--<h1>Our vison</h1>-->
+                        <!--<p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some </p>-->
                         <button>Read More..
                         </button>
                     </div>
                     <div class="index_tre">
-                        <h1>Get a cute teddy bear</h1>
-                        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some </p>
+                         <?php
+                        echo '<form method="POST" action="doUpdate.php">
+                        <input type="text" name="content" value="'.$data["4"]["content"].'">
+                        <input type="hidden" name="id" value="h1-1">
+                        <input type="hidden" name="index" value="index">
+                    </form>';
+                        
+                        
+
+                        echo '<form method="POST" action="doUpdate.php">
+                            <input type="text" style="width:300px; height:270px;" rows="17" cols="40" name="content" value="'.$data["5"]["content"].'">
+                            <input type="hidden" name="id" value="p-1">
+                            <input type="hidden" name="index" value="index">
+                        </form>';
+                                ?>
+                        <!--<h1>Get a cute teddy bear</h1>-->
+                        <!--<p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some </p>-->
                         <button>Read More..
                         </button>
                     </div>
